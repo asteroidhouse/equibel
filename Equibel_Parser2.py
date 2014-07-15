cf794fb17603a333943d70f1e5ffffa098a689a7
Paul Vicol <paul22vicol@gmail.com>
2014-07-14 17:44:36 -0700

    Created Equibel_Parser2.py, which adds support for parsing function calls, constructor calls, and assignment statements. It also has better support for optional whitespace (including optional newlines).

--- Equibel_Parser2.py
+++ Equibel_Parser2.py
@@ -7,7 +7,6 @@ def parse_equibel(text):
      tokens = ("IDENTIFIER", "INTEGER", "STRING", "LPAREN", "RPAREN", "COMMA", "EQUALS",
                "LSQUARE", "RSQUARE", "WHITESPACE", "NEWLINE")
 
-     #t_ORDERED_PAIR = r'\(\s*(0|[1-9][0-9]*)\s*,\s*(0|[1-9][0-9]*)\s*\)'
      t_IDENTIFIER = r'[a-zA-Z][_a-zA-Z0-9]*'
 
      t_LPAREN  = r'\('
@@ -42,16 +41,21 @@ def parse_equibel(text):
           print("failed to parse {0}: {1}".format(t.lineno + 1, line))
 
 
+
      #------------------------------------------------------------------------------------------------------
      #                                                PARSER
      #------------------------------------------------------------------------------------------------------
-
      def p_LINES(p):
           """LINES : LINE NEWLINE
                    | LINE NEWLINE LINES"""
           print("lines")
           p[0] = [p[1]] if len(p) == 3 else [p[1]] + p[3]
 
+     # TODO: Think about semicolon terminators, in terms of:
+     #         1) Signaling the end of a statement, to allow multiple statements
+     #            on the same line, and
+     #         2) Indicating that output should be suppressed when executing the
+     #            line.
      def p_LINE(p):
           """LINE : OPT_WHITE EXPRESSION OPT_WHITE"""
           print("line")
@@ -61,10 +65,14 @@ def parse_equibel(text):
           """EXPRESSION : LITERAL
                         | IDENTIFIER
                         | ASSIGNMENT
-                        | FUNCTION_CALL"""
+                        | FUNCTION_CALL
+                        | CONSTRUCTOR"""
           p[0] = p[1]
 
 
+     # TODO: Think about parenthesized expressions as arguments, particularly
+     #       parenthesized function calls, whose return values are to be used
+     #       as arguments.
      def p_FUNCTION_CALL(p):
           """FUNCTION_CALL : IDENTIFIER WHITESPACE ARGS"""
           p[0] = (p[1], p[3])
@@ -73,6 +81,20 @@ def parse_equibel(text):
           """ARGS : EXPRESSION OPT_WHITE
                   | EXPRESSION WHITESPACE ARGS"""
           p[0] = [p[1]] if len(p) == 3 else [p[1]] + p[3]
+
+     
+     def p_CONSTRUCTOR(p):
+          """CONSTRUCTOR : IDENTIFIER LPAREN OPT_WHITE_NEWLINE RPAREN
+                         | IDENTIFIER LPAREN OPT_WHITE_NEWLINE COMMA_ARGS OPT_WHITE_NEWLINE RPAREN"""
+          if len(p) == 5:
+               p[0] = ('constructor', p[1], [])
+          else:
+               p[0] = ('constructor', p[1], p[4])
+
+     def p_COMMA_ARGS(p):
+          """COMMA_ARGS : EXPRESSION OPT_WHITE_NEWLINE
+                        | EXPRESSION OPT_WHITE_NEWLINE COMMA OPT_WHITE_NEWLINE COMMA_ARGS"""
+          p[0] = [p[1]] if len(p) == 3 else [p[1]] + p[5]
 
 
      def p_ASSIGNMENT(p):
@@ -88,13 +110,14 @@ def parse_equibel(text):
           p[0] = p[1]
 
      def p_ORDERED_PAIR(p):
-          """ORDERED_PAIR : LPAREN OPT_WHITE INTEGER OPT_WHITE COMMA OPT_WHITE INTEGER OPT_WHITE RPAREN"""
+          """ORDERED_PAIR : LPAREN OPT_WHITE_NEWLINE INTEGER OPT_WHITE_NEWLINE COMMA \
+                            OPT_WHITE_NEWLINE INTEGER OPT_WHITE_NEWLINE RPAREN"""
           print("pair")
           p[0] = (int(p[3]), int(p[7]))
 
      def p_LIST(p):
-          """LIST : LSQUARE OPT_WHITE RSQUARE
-                  | LSQUARE OPT_WHITE ELEMENTS OPT_WHITE RSQUARE"""
+          """LIST : LSQUARE OPT_WHITE_NEWLINE RSQUARE
+                  | LSQUARE OPT_WHITE_NEWLINE ELEMENTS OPT_WHITE_NEWLINE RSQUARE"""
           if len(p) == 4:
                print("empty list")
                p[0] = []
@@ -103,8 +126,8 @@ def parse_equibel(text):
                p[0] = p[3]
 
      def p_ELEMENTS(p):
-          """ELEMENTS : ELEMENT OPT_WHITE
-                      | ELEMENT OPT_WHITE COMMA OPT_WHITE ELEMENTS"""
+          """ELEMENTS : ELEMENT OPT_WHITE_NEWLINE
+                      | ELEMENT OPT_WHITE_NEWLINE COMMA OPT_WHITE_NEWLINE ELEMENTS"""
           print("elements")
           p[0] = [p[1]] if len(p) == 3 else [p[1]] + p[5]
 
@@ -116,6 +139,18 @@ def parse_equibel(text):
                      | ORDERED_PAIR"""
           print("element")
           p[0] = p[1]
+
+     def p_OPT_WHITE_NEWLINE(p):
+          """OPT_WHITE_NEWLINE : WHITESPACE_NEWLINE
+                               | empty"""
+          print("optional whitespace/newlines")
+
+     def p_WHITESPACE_NEWLINE(p):
+          """WHITESPACE_NEWLINE : WHITESPACE
+                                | NEWLINE
+                                | WHITESPACE WHITESPACE_NEWLINE
+                                | NEWLINE WHITESPACE_NEWLINE"""
+          print("whitespace or newlines")
 
      def p_OPT_WHITE(p):
           """OPT_WHITE : WHITESPACE
@@ -133,19 +168,12 @@ def parse_equibel(text):
 
      lexer = ply.lex.lex()
      parser = ply.yacc.yacc()
-     #lexer.input(text)
 
      try:
-          result = parser.parse(text, lexer=lexer)
-          return result
+          return parser.parse(text, lexer=lexer)
      except ValueError as err:
           print(err)
           return []
-
-     """
-     for token in lexer:
-          print(token)
-     """
 
 if __name__ == '__main__':
      if len(sys.argv) < 2:
