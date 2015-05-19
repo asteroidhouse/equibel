cb9423cdd739a3746338f405f2140da357614a1d
Paul Vicol <paul22vicol@gmail.com>
2015-05-18 21:26:24 -0700

    Modified __init__.py to load submodules when equibel is imported

--- equibel/__init__.py
+++ equibel/__init__.py
@@ -2,3 +2,16 @@ from __future__ import absolute_import
 
 import equibel.equibel
 from equibel.equibel import *
+
+import equibel.graph
+from equibel.graph import *
+
+import equibel.generator
+from equibel.generator import *
+
+import equibel.graph_manager
+from equibel.graph_manager import *
+
+import equibel.solver
+from equibel.solver import *
+
