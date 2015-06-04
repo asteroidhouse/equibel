7e1d641888fc19b07caf95e4e99c0a79861d1e6a
Paul Vicol <paul22vicol@gmail.com>
2015-06-03 19:58:24 -0700

    Added timing functions for external clingo calls

--- tests/asp_length_tests.py
+++ tests/asp_length_tests.py
@@ -6,15 +6,18 @@ from __future__ import print_function
 import random
 import time
 
+from timeit import timeit
+
+import subprocess
 from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
+
 import equibel as eb
 
 
 PATH = 'path'
 STAR = 'star'
 COMPLETE = 'complete'
-graph_types = [PATH, STAR, COMPLETE]
 
 
 def generate_formula(num_variables):
@@ -34,6 +37,7 @@ def generate_formula(num_variables):
     
     #return eb.simplify(formula)
     return formula
+
 
 def populate_formulas(G, num_variables):
     # Generate formulas for each node according to certain parameters
@@ -60,7 +64,52 @@ def write_graph(graph, filename):
     f.write(asp_str)
     f.close()
 
-def count_lines(start_num_nodes, end_num_nodes, step_size, graph_type, num_variables=4):
+
+def time_command(command):
+    print("Starting \"{0}\"".format(command))
+    start_time = time.clock()
+    proc = Popen(command, shell=True, stdout=PIPE, universal_newlines=True)
+    #output = proc.stdout.read()
+    proc.terminate()
+    #print("Output = ", output)
+    end_time = time.clock()
+    print("Ending \"{0}\"".format(command))
+    print("It took {0}".format(end_time - start_time))
+    return end_time - start_time
+
+
+def solving_time(filename, prefix, eq=False, transitive=False, translate=False):
+    command = "clingo {0}".format(filename)
+    if eq:
+        command += " {pre}/eq_sets.lp ".format(pre=prefix)
+    if transitive:
+        command += " {pre}/transitive.lp ".format(pre=prefix)
+    if translate:
+        command += " {pre}/translate.lp ".format(pre=prefix)
+
+    command += " 0 --heuristic=domain --enum-mode=domRec "
+
+    return time_command(command)
+    
+
+def count_lines(filename, prefix, eq=False, transitive=False, translate=False):
+    command = "gringo {0}".format(filename)
+
+    if eq:
+        command += " {pre}/eq_sets.lp ".format(pre=prefix)
+    if transitive:
+        command += " {pre}/transitive.lp ".format(pre=prefix)
+    if translate: 
+        command += " {pre}/translate.lp ".format(pre=prefix)
+
+    command += " | wc -l"
+
+    proc = Popen(command.format(filename), shell=True, stdout=PIPE, universal_newlines=True)
+    graph_lines = int(proc.stdout.read())
+    return graph_lines
+
+
+def lines_and_solving_time(start_num_nodes, end_num_nodes, step_size, graph_type, num_variables=4):
     prefix = "../equibel/asp"
     counts = dict()
 
@@ -71,38 +120,47 @@ def count_lines(start_num_nodes, end_num_nodes, step_size, graph_type, num_varia
         filename = "temp_asp_file"
         write_graph(G, filename)
 
-        proc = Popen("gringo {0} | wc -l".format(filename), shell=True, stdout=PIPE, universal_newlines=True)
-        graph_lines = int(proc.stdout.read())
+        graph_lines = count_lines(filename, prefix)
+        graph_time = solving_time(filename, prefix)
 
-        proc = Popen("gringo {0} {pre}/eq_sets.lp | wc -l".format(filename, pre=prefix), 
-                     shell=True, stdout=PIPE, universal_newlines=True)
-        graph_eq_lines = int(proc.stdout.read())
+        graph_eq_lines = count_lines(filename, prefix, eq=True)
+        graph_eq_time = solving_time(filename, prefix, eq=True)
 
-        proc = Popen("gringo {0} {pre}/eq_sets.lp {pre}/transitive.lp | wc -l".format(filename, pre=prefix), 
-                     shell=True, stdout=PIPE, universal_newlines=True)
-        graph_eq_trans_lines = int(proc.stdout.read())
+        graph_eq_trans_lines = count_lines(filename, prefix, eq=True, transitive=True)
+        graph_eq_trans_time = solving_time(filename, prefix, eq=True, transitive=True)
 
-        proc = Popen("gringo {0} {pre}/eq_sets.lp {pre}/transitive.lp {pre}/translate.lp | wc -l".format(filename, pre=prefix), 
-                     shell=True, stdout=PIPE, universal_newlines=True)
-        graph_eq_t2_lines = int(proc.stdout.read())
+        graph_eq_t2_lines = count_lines(filename, prefix, eq=True, transitive=True, translate=True)
+        graph_eq_t2_time = solving_time(filename, prefix, eq=True, transitive=True, translate=True)
+
 
         counts[num_nodes] = (graph_lines, graph_eq_lines, graph_eq_trans_lines, graph_eq_t2_lines)
         
+        print("Num Lines in Ground Representation")
         print("--------------------------------------------------")
         print("# Nodes = {0}".format(num_nodes))
         print("GRAPH = {0}".format(graph_lines))
         print("GRAPH + EQ = {0}".format(graph_eq_lines))
         print("GRAPH + EQ + TRANSITIVE = {0}".format(graph_eq_trans_lines))
         print("GRAPH + EQ + TRANSITIVE + TRANSLATE = {0}".format(graph_eq_t2_lines))
+        print("==================================================")
+        print()
+
+        print("Solving Times")
+        print("--------------------------------------------------")
+        print("# Nodes = {0}".format(num_nodes))
+        print("GRAPH = {0}".format(graph_time))
+        print("GRAPH + EQ = {0}".format(graph_eq_time))
+        print("GRAPH + EQ + TRANSITIVE = {0}".format(graph_eq_trans_time))
+        print("GRAPH + EQ + TRANSITIVE + TRANSLATE = {0}".format(graph_eq_t2_time))
 
     return counts
 
 if __name__ == '__main__':
     start_num_nodes = 5
-    end_num_nodes = 35
+    end_num_nodes = 20
     step_size = 5
 
-    counts = count_lines(start_num_nodes, end_num_nodes, step_size, COMPLETE)
+    counts = lines_and_solving_time(start_num_nodes, end_num_nodes, step_size, STAR)
     plt.plot(range(start_num_nodes, end_num_nodes+1, step_size), [counts[val][0] for val in counts], 'go-')
     plt.plot(range(start_num_nodes, end_num_nodes+1, step_size), [counts[val][1] for val in counts], 'bo-')
     plt.plot(range(start_num_nodes, end_num_nodes+1, step_size), [counts[val][2] for val in counts], 'ro-')
