1a6b8c5d311925ae9cbe15618a057667fa0babd1
Paul Vicol <paul22vicol@gmail.com>
2015-07-03 15:41:33 -0700

    Changed to version 0.8.8a1 for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='equibel',
-    version='0.8.7a1', # v0.8.7 alpha build
+    version='0.8.8a1', # v0.8.8 alpha build
     
     description='A toolkit for equivalence-based belief change',
 
