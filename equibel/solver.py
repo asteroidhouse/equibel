365e1f5420ccd0ffe7454fee1ab3fb2a3649fac9
Paul Vicol <paul22vicol@gmail.com>
2015-07-16 21:33:10 -0700

    Revamped solver to be faster and more memory-efficient

--- equibel/solver.py
+++ equibel/solver.py
@@ -11,6 +11,9 @@ import pkg_resources
 import copy
 import platform
 
+import functools
+from functools import wraps
+
 import equibel
 
 if platform.system() == 'Linux':
@@ -39,83 +42,55 @@ CARDINALITY = 'cardinality'
 
 EQ_SETS_FILE = pkg_resources.resource_filename('equibel', 'asp/eq_sets.lp')
 TRANSITIVE_FILE = pkg_resources.resource_filename('equibel', 'asp/transitive.lp')
-TRANSLATE_FILE = pkg_resources.resource_filename('equibel', 'asp/translate.lp')
-EQ_EXPANDING_FILE = pkg_resources.resource_filename('equibel', 'asp/eq_expanding.lp')
+#TRANSLATE_FILE = pkg_resources.resource_filename('equibel', 'asp/translate.lp')
+#EQ_EXPANDING_FILE = pkg_resources.resource_filename('equibel', 'asp/eq_expanding.lp')
 
 
 class EqSolver(object):
-
     def __init__(self):
         self.optimal_models = set()
-        self.optimal_values = None
+        self.eq_dicts = []
+        self.graph = None
+        self.conjunctions = dict()
+        self.disjunctions = dict()
 
-    def find_models(self, asp_string, method=CONTAINMENT):
-        #print("ASP STRING = {0}".format(asp_string))
+    def conjunction(formulas):
+        result = Prop(True)
+        for formula in formulas:
+            result &= formula
+        return result
+
+
+    def disjunction(formulas):
+        result = Prop(False)
+        for formula in formulas:
+            result |= formula
+        return result
+    
+    def find_completion(self, G, method=CONTAINMENT):
         self.optimal_models = set()
-        self.optimal_values = None
+        self.eq_dicts = []
+        self.graph = G
 
         ctl = gringo.Control()
         ctl.load(EQ_SETS_FILE)
         ctl.load(TRANSITIVE_FILE)
-
         self._configure_control(ctl, method)
 
-        ctl.add('base', [], asp_string)
+        ctl.add('base', [], equibel.convert_to_asp(G))
         ctl.ground([('base', [])])
-        ctl.solve(on_model=self.capture_optimal_models)
-
-        #print("SOLVER MODELS = {0}".format(self.optimal_models))
-        print("NUM EQ SETS = {0}".format(len(self.optimal_models)))
-        return self.optimal_models
-
-
-    def one_shot_eq(self, asp_string, method=CONTAINMENT):
-        models = self.find_models(asp_string, method)
-        return self.extract_eq(models)
-
-    
-    def extract_eq(self, models):
-        print("STARTED EXTRACTING EQ INFO")
-        eq_dicts = []
-        for model in models:
-            eq_dict = dict()
-            for term in model:
-                if term.name() == 'eq':
-                    atom = str(term.args()[0])
-                    node1 = term.args()[1]
-                    node2 = term.args()[2]
-                    if node2 not in eq_dict:
-                        eq_dict[node2] = dict()
-                        eq_dict[node2][node1] = [atom]
-                    else:
-                        if node1 not in eq_dict[node2]:
-                            eq_dict[node2][node1] = [atom]
-                        else:
-                            eq_dict[node2][node1].append(atom)
-            eq_dicts.append(eq_dict)
-        print("FINISHED EXTRACTING EQ INFO")
-        return eq_dicts
-    
-
-    def one_shot_dicts(self, asp_string, method=CONTAINMENT):
-        models = self.find_models(asp_string, method)
-        return self.model_dicts(models)
-
-
-    def model_dicts(self, models):
-        model_list = []
-
-        for model in models:
-            term_dict = dict()
-            for term in model:
-                name = term.name()
-                if name in term_dict:
-                    term_dict[name].append(term)
-                else:
-                    term_dict[name] = [term]
-            model_list.append(term_dict)
-
-        return model_list
+        ctl.solve(on_model=self.on_model)
+        
+        for node in self.conjunctions:
+            self.disjunctions[node] = disjunction(self.conjunctions[node])
+            #print("DISJUNCTION NODE {0} = {1}".format(node, self.disjunctions[node]))
+            
+        R = copy.deepcopy(G)
+        for node in R:
+            new_information = self.disjunctions[node]
+            updated_formula = conjunction([form for form in G.formulas(node)] + [new_information])
+            R.set_formulas(node, [simplify(updated_formula)])
+        return R
 
     def _configure_control(self, ctl, method):
         if method == CONTAINMENT:
@@ -127,130 +102,79 @@ class EqSolver(object):
             ctl.conf.solve.opt_mode   = 'optN'
             ctl.conf.solve.enum_mode  = 'auto'
             ctl.conf.solver.heuristic = 'none'
-
-    def capture_optimal_models(self, model):
-        model_atoms = frozenset(model.atoms(Model.SHOWN))
-        opt_values = model.optimization()
-
-        #print("Opt val = {0}".format(self.optimal_values))
-
-        if self.optimal_values is None:
-            self.optimal_values = opt_values
-            self.optimal_models.add(model_atoms)
-        elif opt_values == self.optimal_values:
-            self.optimal_models.add(model_atoms)
-        elif opt_values < self.optimal_values:
-            self.optimal_values = opt_values
-            self.optimal_models.clear()
-            self.optimal_models.add(model_atoms)
-
-
-"""
-# TEST TEST TEST
-def completion(G, solving_method=CONTAINMENT):
-    solver = EqSolver()
-    models = solver.one_shot_dicts(ASP_Formatter.convert_to_asp(G), solving_method)
-    #print("MODELS = {0}".format(models))
-    node_formulas = FormulaExtractor.combine_formulas(models)
-
-    R = copy.deepcopy(G)
-
-    # TODO: Decide whether to always represent "multiple formulas" as a
-    # single conjunction, or whether to keep them separate.
-    for node_id in R.nodes():
-        if node_id in node_formulas:
-            new_formula = node_formulas[node_id]
-            old_formula = conjunction(R.formulas(node_id))
-            R.set_formulas(node_id, [simplify(old_formula & new_formula)])
-            #R.set_formulas(node_id, [old_formula & new_formula])
-
-    return R
-"""
+    
+    def on_model(self, model):
+        #print(model.atoms(Model.SHOWN))
+        eq_dict = dict()
+        for term in model.atoms(Model.SHOWN):
+            if term.name() == 'eq':
+                (atom_fun, node1, node2) = term.args()
+                atom = str(atom_fun)
+                if node1 not in eq_dict:
+                    eq_dict[node1] = dict()
+                    eq_dict[node1][node2] = set([atom])
+                else:
+                    if node2 not in eq_dict[node1]:
+                        eq_dict[node1][node2] = set([atom])
+                    else:
+                        eq_dict[node1][node2].add(atom)
+        
+        for node1 in eq_dict:
+            for node2 in eq_dict[node1]:
+                eq_dict[node1][node2] = frozenset(eq_dict[node1][node2])
+        
+        self.translate_using_eq(eq_dict)
+    
+    def translate_using_eq(self, eq):
+        for node in self.graph:
+            other_nodes = [other for other in self.graph if other != node]
+            formulas = set()
+            for other_node in other_nodes:
+                if node < other_node:
+                    smaller_node = node
+                    bigger_node = other_node
+                else:
+                    smaller_node = other_node
+                    bigger_node = node
+                if smaller_node in eq:
+                    if bigger_node in eq[smaller_node]:
+                        eq_atoms = eq[smaller_node][bigger_node]
+                        other_node_formulas = self.graph.formulas(other_node)
+                        if other_node_formulas:
+                            trans = translate_formulas(other_node_formulas, eq_atoms)
+                            formulas.update(trans)
+                            #print("FORMULAS = {0}".format(formulas))
+            
+            if node not in self.conjunctions:
+                self.conjunctions[node] = set([conjunction(formulas)])
+            else:
+                self.conjunctions[node].add(conjunction(formulas))
 
 
 def conjunction(formulas):
     result = Prop(True)
-
     for formula in formulas:
-        #print("FORMULA : {0}".format(formula))
         result &= formula
-
-    """
-    res = simplify(result)
-    if debug:
-        print("SIMPLIFIED CONJUNCTION = {0}".format(res))
-    return res
-    """
     return result
 
 
 def disjunction(formulas):
     result = Prop(False)
-
     for formula in formulas:
         result |= formula
-
-    #return simplify(result)
     return result
 
 debug = False
 
-def completion(G, solving_method=CONTAINMENT):
-    #print("COMPLETION")
-    solver = EqSolver()
-    eq_dicts = solver.one_shot_eq(equibel.convert_to_asp(G), solving_method)
-    if debug:
-        print("EQ DICTS = {0}".format(eq_dicts))
-        print("NUMBER OF EQ DICTS = {0}".format(len(eq_dicts)))
-    return final_formulas(G, eq_dicts)
-
-
-def final_formulas(G, eq_dicts):
-    if debug:
-        print("In final formulas...")
-        print("-------------------------------")
-    R = copy.deepcopy(G)
-    for node in R.nodes():
-        res = new_formula(node, G, eq_dicts)
-        if debug:
-            print("NODE {0} NEW FORMULAS= {1}".format(node, repr(res)))
-        new_form = conjunction([form for form in G.formulas(node)] + [res])
-        if debug:
-            print("NODE {0} CONJUNCTION OF NEW FORMULAS= {1}".format(node, repr(new_form)))
-        R.set_formulas(node, [simplify(new_form)])
-        #R.set_formulas(node, [new_form])
-    return R
-
-
-def new_formula(node, G, eq_dicts):
-    if debug:
-        print("In new formulas...")
-        print("-------------------------------")
-    conjunctions = set()
+def print_eq_dicts(eq_dicts):
     for eq_dict in eq_dicts:
-        formulas = []
-        if node in eq_dict:
-            other_nodes = [curr_node for curr_node in G.nodes() if curr_node != node]
-            if debug:
-                print("\tUsing EQ DICT: {0}".format(eq_dict))
-                print("\t-------------------------------")
-            for other_node in other_nodes:
-                if debug:
-                    print("\t\tTranslating from {0} -> {1}".format(other_node, node))
-                    print("\t\t-------------------------------")
-                #eq_atoms = eq_dict[node][other_node]
-                eq_atoms = eq_dict[node].get(other_node, [])
-                if debug:
-                    print("\t\t\tEQ atoms = {0}".format(eq_atoms))
-                formulas.append(translate_formulas(G.formulas(other_node), eq_atoms))
-                if debug:
-                    print("\t\t\tFormulas = {0}".format(formulas))
-            conjunctions.add(conjunction([form for forms in formulas for form in forms]))
-            if debug:
-                print("\tCONJUNCTIONS = {0}".format(conjunctions))
+        for key in eq_dict:
+            print("{0} => {1}".format(key, eq_dict[key]))
+        print("\n")
 
-    return simplify(disjunction(conjunctions))
-    #return disjunction(conjunctions)
+def completion(G, solving_method=CONTAINMENT):
+    solver = EqSolver()
+    return solver.find_completion(G)
 
 
 def translate_formulas(formulas, eq_atoms):
@@ -269,9 +193,18 @@ def translate_formulas(formulas, eq_atoms):
     return translated_formulas
 
 
+def memo(func):
+    cache = {}
+    @wraps(func)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrap
+
+
+@memo
 def translate_formula(formula, eq_atoms):
-    if debug:
-        print("\t\t\t\t\tTranslating {0} with {1}".format(repr(formula), eq_atoms))
     if formula.is_atomic():
         if formula.get_name() in eq_atoms:
             return formula
@@ -332,31 +265,16 @@ def con_merge(belief_bases, entailment_based_constraints=None, consistency_based
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("usage: python solver.py NUM_NODES")
+        sys.exit(1)
+    
+    num_nodes = int(sys.argv[1])
+    
     solver = EqSolver()
-
-    G = equibel.path_graph(20)
-    G.add_formula(0, "p & q")
-    G.add_formula(1, "a & b & c & d & e & f & g & h & i & j & ~p")
-    #eq_dicts = solver.one_shot_eq(equibel.convert_to_asp(G), method=CONTAINMENT)
-    #print(eq_dicts)
-
-    #print(eq_dicts)
-    #print("NEW FORMULA {0}".format(new_formulas(0, G, eq_dicts)))
-
-    """
-    formula = equibel.parse_infix_formula("(p & q) | r")
-    eq_atoms = ['q']
-    translated = translate_formula(formula, eq_atoms)
-    print(repr(translated))
-    """
-
+    G = equibel.path_graph(num_nodes)
+    G.add_formula(0, "p & q & r & s")
+    G.add_formula(num_nodes-1, "~p & ~q & ~r & ~s")
     R = completion(G)
     for node_id in R.nodes():
         print("Node {0}, formulas = {1}".format(node_id, R.formulas(node_id)))
-
-
-    #filename = sys.argv[1]
-    #f = file(filename, 'r')
-    #final_models = solver.one_shot_dicts(equibel.convert_to_asp(G), method=CONTAINMENT)
-    #print(final_models)
-
