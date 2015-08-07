134e521a68164d68c2e557cf54c0335de1fc2e97
Paul Vicol <paul22vicol@gmail.com>
2015-08-06 23:10:15 -0700

    Added documentation for the graph and generator classes

--- equibel/generator.py
+++ equibel/generator.py
@@ -11,19 +11,22 @@ the corresponding networkx graphs.
 from __future__ import absolute_import
 from __future__ import print_function
 
-import networkx as nx
 import sys
+
+import networkx as nx
 
 from equibel.graph import EquibelGraph
 
 
 # ======================================================
+# 
 # Classic Graph Generators -- Line, Star, Complete, etc.
+# 
 # ======================================================
 
 def path_graph(n, directed=False):
     """Create a directed or undirected line graph on n nodes, 
-    numbered using the integers 1 to n, inclusive.
+    numbered using the integers 0 to n-1, inclusive.
 
     Parameters
     ----------
@@ -40,10 +43,10 @@ def path_graph(n, directed=False):
 
     Examples
     --------
-    Create an undirected line graph 1 <-> 2 <-> 3 <-> 4:
+    Create an undirected line graph 0 <-> 1 <-> 2 <-> 3:
     >>> G = line_graph(4)
 
-    Create a directed line graph 1 -> 2 -> 3 -> 4:
+    Create a directed line graph 0 -> 1 -> 2 -> 3:
     >>> D = line_graph(4, directed=True)
     """
     if directed:
@@ -73,12 +76,30 @@ def complete_graph(n):
 def star_graph(n):
     """Create an undirected star graph on n+1 nodes, with one 
     central node, and n outer nodes.
+
+    Parameters
+    ----------
+    n : int
+        The desired number of outer nodes for the star graph.
+        The generated graph will have a central node numbered 
+        0, and n outer nodes numbered from 1 to n.
+
+    Returns
+    -------
+    G : EquibelGraph
+        An EquibelGraph containing a star graph on n+1 nodes.
+
+    Examples
+    --------
+    >>> G = star_graph(5)
     """
     return EquibelGraph(nx.star_graph(n))
 
 
 # ========================================================
+# 
 # Random Graph Generators -- Waxman, Barabasi-Albert, etc.
+# 
 # ========================================================
 
 def waxman_graph(n, alpha=0.4, beta=0.1, L=None, domain=(0,0,1,1)):
--- equibel/graph.py
+++ equibel/graph.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import networkx as nx
+
 import equibel
 
 ATOMS_KEY    = "atoms"
@@ -39,23 +40,89 @@ class EquibelGraph:
     # ================================================
 
     def nodes(self):
+        """Return the list of nodes.
+        """
         return self.graph.nodes()
 
     def add_node(self, node_id):
+        """Add a node to the graph.
+        
+        Parameters
+        ----------
+        node_id : any type, usually an int
+
+        Examples
+        --------
+        Create a graph:
+        >>> G = Graph()
+
+        Add a node:
+        >>> G.add_node(1)
+        """
         self.graph.add_node(node_id)
         self.graph.node[node_id][WEIGHTS_KEY]  = dict()
         self.graph.node[node_id][FORMULAS_KEY] = set()
         self.__add_atoms_to_new_node(node_id)
 
     def add_nodes(self, nodes):
+        """Add all nodes from an iterable container.
+
+        Parameters
+        ----------
+        nodes : any iterable container
+
+        Example
+        -------
+        
+        """
         for node in nodes:
             self.add_node(node)
 
     def remove_node(self, node_id):
+        """Remove a node from the graph.
+
+        Parameters
+        ----------
+        node_id : any type, usually an int
+
+        Example
+        -------
+        >>> G = Graph()
+        >>> G.add_nodes([1,2,3])
+        >>> G.remove_node(1)
+        >>> G.nodes()
+        [2,3]
+        """
         self.graph.remove_node(node_id)
 
     # Perhaps this could be called "remove_nodes"
     def remove_nodes_from(self, nodes):
+        """Remove nodes specified in an iterable container.
+
+        Parameters
+        ----------
+        nodes : any iterable container
+
+        Example
+        -------
+        >>> G = Graph()
+        >>> G.add_nodes([1,2,3,4])
+        >>> G.nodes()
+        [1,2,3,4]
+        >>> G.remove_nodes_from([1,2])
+        >>> G.nodes()
+        [2,4]
+
+        Nodes in the iterable that are not in the graph 
+        are ignored:
+
+        >>> G.add_nodes([3,4])
+        >>> G.nodes()
+        [1,2,3,4]
+        >>> G.remove_nodes_from([4,5,6])
+        >>> G.nodes()
+        [1,2,3]
+        """
         self.graph.remove_nodes_from(nodes)
 
     # ================================================
@@ -64,9 +131,39 @@ class EquibelGraph:
 
     # ROUTING to G.edges()
     def edges(self):
+        """Returns a list of edges of the graph.
+
+        Example
+        -------
+        >>> G = equibel.path_graph(4)
+        >>> G.edges()
+        [(1,2), (2,3), (3,4)]
+        """
         return self.graph.edges()
 
     def add_edge(self, from_node_id, to_node_id):
+        """Adds an edge to the graph. If an endpoint
+        of the edge is not already in the graph, it 
+        is added as a new node automatically. So, an 
+        easy way to create a graph is to just add 
+        the desired edges; the necessary nodes will 
+        be created for you.
+
+        Parameters
+        ----------
+        from_node_id : any type, usually an int
+        to_node_id   : any type, usually an int
+
+        Examples
+        -------
+        >>> G = Graph()
+        >>> G.add_edge(1,2)
+        >>> G.add_edge(1,3)
+        >>> G.edges()
+        [(1,2), (2,3)]
+        >>> G.nodes()
+        [1,2,3]
+        """
         if from_node_id not in self.graph:
             self.add_node(from_node_id)
         if to_node_id not in self.graph:
@@ -75,22 +172,80 @@ class EquibelGraph:
         self.graph.add_edge(from_node_id, to_node_id)
 
     def add_edges(self, edges):
+        """Adds edges to the graph from an iterable container.
+        Any endpoint of an edge that is not already in the list 
+        of nodes will be automatically added as a node.
+
+        Parameters
+        ----------
+        edges : any iterable consisting of pairs (2-tuples)
+
+        Examples
+        --------
+        >>> G = Graph()
+        >>> G.add_edges([(1,2), (2,3), (3,4)])
+        >>> G.edges()
+        [(1,2), (2,3), (3,4)]
+        >>> G.nodes()
+        [1,2,3,4]
+        """
         for (from_node_id, to_node_id) in edges:
             self.add_edge(from_node_id, to_node_id)
 
     def remove_edge(self, from_node_id, to_node_id):
+        """Removes an edge from the graph, if it exists. 
+        Otherwise, does nothing. This does not remove 
+        any nodes that were added by the add_edge or 
+        add_edges functions.
+
+        Parameters
+        ----------
+        from_node_id : any type, usually an int
+        to_node_id   : any type, usually an int
+
+        Examples
+        --------
+        >>> G = Graph()
+        >>> G.add_edges([(1,2), (1,3), (1,4)])
+        >>> G.edges()
+        [(1,2), (1,3), (1,4)]
+        >>> G.nodes()
+        [1,2,3,4]
+        >>> G.remove_edge(1,4)
+        >>> G.edges()
+        [(1,2), (1,3)]
+        >>> G.nodes()
+        [1,2,3,4]
+        """
         self.graph.remove_edge(from_node_id, to_node_id)
 
     def to_directed(self):
+        """Creates a directed copy of this graph.
+
+        """
         return EquibelGraph(self.graph.to_directed())
 
     def to_undirected(self):
+        """Creates an undirected copy of this graph.
+        """
         return EquibelGraph(self.graph.to_undirected())
 
     def is_directed(self):
+        """Checks whether this graph is directed.
+
+        Example
+        -------
+        >>> G = Graph()
+        >>> 
+        """
         return self.graph.is_directed()
 
+    # TODO: What if the graph is undirected? 
     def reverse(self):
+        """Creates a copy of this graph with the directions 
+        of all edges reversed.
+
+        """
         return EquibelGraph(self.graph.reverse())
 
     # ================================================
@@ -99,10 +254,24 @@ class EquibelGraph:
 
     # Instead of G.graph[ATOMS_KEY], use G.atoms()
     def atoms(self):
+        """Returns a list of atoms that appear somewhere 
+        in the graph. That is, this returns a list of atoms 
+        used by at least one formula of one node in the graph.
+
+        Example
+        -------
+        >>> G = Graph()
+        >>> G.add_node(1)
+        >>> G.add_formula(1, "p & q")
+        >>> G.atoms()
+        ['p', 'q']
+        """
+        # TODO: Is this how atoms are returned? Strings?
         return self.graph.graph[ATOMS_KEY]
 
     # Instead of G.node[node_id][WEIGHTS_KEY], use G.atom_weights(node_id)
     def atom_weights(self, node_id):
+        """
         return self.graph.node[node_id][WEIGHTS_KEY]
 
     def weight(self, node_id, atom):
