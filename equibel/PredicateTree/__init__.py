1b284b870b57fc971600a0d8748a89d7b5f03d7a
Paul Vicol <paul22vicol@gmail.com>
2015-08-18 14:13:46 -0700

    Deleted un-necessary files

--- equibel/PredicateTree/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-import equibel.PredicateTree.predicate
-from equibel.PredicateTree.predicate import *
-
-import equibel.PredicateTree.predicate_parser
-from equibel.PredicateTree.predicate_parser import *
--- equibel/PredicateTree/predicate.py
+++
@@ -1,7 +0,0 @@
-class Predicate:
-    def __init__(self, name, children=[]):
-        self.name = name
-        self.children = children
-
-    def __str__(self):
-        return "{0}({1})".format(self.name, ",".join([str(child) for child in self.children]))
