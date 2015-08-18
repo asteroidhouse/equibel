341185962f8618a3e3b23911cfafcebb614ebb56
Paul Vicol <paul22vicol@gmail.com>
2015-08-18 14:59:15 -0700

    Now returns the number of iterations needed to reach a steady state

--- equibel/solver.py
+++ equibel/solver.py
@@ -1,12 +1,18 @@
+"""
+"""
+#    Copyright (C) 2014-2015 by
+#    Paul Vicol <pvicol@sfu.ca>
+#    All rights reserved.
+#    MIT license.
 from __future__ import absolute_import
 from __future__ import print_function
 
 import copy
-import sys
 from collections import defaultdict
 from functools import wraps
 import platform
 import pkg_resources
+import sys
 
 import gringo
 
@@ -27,9 +33,7 @@ elif platform.system() == 'Darwin':
     import equibel.includes.mac.gringo as gringo
     from equibel.includes.mac.gringo import Control, Model, Fun
 
-import equibel.FormulaExtractor as FormulaExtractor
 import equibel.formatters.ASP_Formatter as ASP_Formatter
-#from equibel.simbool.simplify import simplify
 
 EQ_SETS_FILE = pkg_resources.resource_filename('equibel', 'asp/eq_sets.lp')
 EQ_ITERATE_FILE = pkg_resources.resource_filename('equibel', 'asp/eq_iterate.lp')
@@ -44,17 +48,20 @@ def create_atom_mapping(atoms):
         mapping[atom] = index
     return mapping
 
+
 def iterate_steady(G):
     atoms = [eb.Prop(atom) for atom in G.atoms()]
     sorted_atoms = tuple(sorted(atoms))
 
     atom_mapping = create_atom_mapping(sorted_atoms)
 
+    iterations = 0
+
     old_R = None
     R = copy.deepcopy(G)
 
-    # TODO: Have a method of testing the equality of graphs
     while old_R != R:
+        iterations += 1
         old_R = copy.deepcopy(R)
         ctl = gringo.Control()
         ctl.conf.configuration = 'handy'
@@ -64,7 +71,6 @@ def iterate_steady(G):
         #ctl.conf.solve.parallel_mode = 2
 
         ctl.load(EQ_ITERATE_FILE)
-        print(eb.convert_to_asp(R, atom_mapping))
         ctl.add('base', [], eb.convert_to_asp(R, atom_mapping))
         ctl.ground([('base', [])])
 
@@ -83,19 +89,20 @@ def iterate_steady(G):
            
             for node in node_tv_dict:
                 node_models[node].add(node_tv_dict[node])
-        print(node_models)
+        #print(node_models)
 
         true_prop = eb.Prop(True)
 
         for node in node_models:
             t = tuple(node_models[node])
             formula = formula_from_models(t, sorted_atoms)
+            # TODO: Do something about this simplification part... it works 
+            #       but it surely decreases efficiency.
             simp = simplified(formula)
-            print("Node {0}: {1}".format(node, repr(simp)))
             if simp != true_prop:
                 R.set_formulas(node, [simp])
 
-    return R
+    return R, iterations
 
 
 def iterate(G, num_iterations):
@@ -115,7 +122,6 @@ def iterate(G, num_iterations):
         #ctl.conf.solve.parallel_mode = 2
 
         ctl.load(EQ_ITERATE_FILE)
-        print(eb.convert_to_asp(R, atom_mapping))
         ctl.add('base', [], eb.convert_to_asp(R, atom_mapping))
         ctl.ground([('base', [])])
 
@@ -134,19 +140,20 @@ def iterate(G, num_iterations):
            
             for node in node_tv_dict:
                 node_models[node].add(node_tv_dict[node])
-        print(node_models)
+        #print(node_models)
 
         true_prop = eb.Prop(True)
 
         for node in node_models:
             t = tuple(node_models[node])
             formula = formula_from_models(t, sorted_atoms)
+            # TODO: Again, formulas are simplified, yielding slowdowns.
             simp = simplified(formula)
-            print("Node {0}: {1}".format(node, repr(simp)))
             if simp != true_prop:
                 R.set_formulas(node, [simp])
 
     return R
+
 
 def completion(G, debug=False, method=CONTAINMENT):
     atoms = [eb.Prop(atom) for atom in G.atoms()]
@@ -155,7 +162,7 @@ def completion(G, debug=False, method=CONTAINMENT):
     atom_mapping = create_atom_mapping(sorted_atoms)
 
     ctl = gringo.Control()
-    #ctl.conf.configuration = 'handy'
+    ctl.conf.configuration = 'handy'
     ctl.conf.solver.heuristic = 'domain'
     ctl.conf.solve.enum_mode = 'domRec'
     ctl.conf.solve.models = 0
@@ -189,10 +196,7 @@ def completion(G, debug=False, method=CONTAINMENT):
        
         for node in node_tv_dict:
             node_models[node].add(node_tv_dict[node])
-    print(node_models)
-
-    #print("Number of models: {0}".format(num_models))
-    #print("Number of terms per model: {0}".format(num_terms_list))
+    #print(node_models)
 
     true_prop = eb.Prop(True)
     
@@ -200,9 +204,7 @@ def completion(G, debug=False, method=CONTAINMENT):
     for node in node_models:
         t = tuple(node_models[node])
         formula = formula_from_models(t, sorted_atoms)
-        #print(repr(formula))
         simple = eb.simplify(formula)
-        #simple = simplified(formula)
         if simple != true_prop:
             R.set_formulas(node, [simple])
 
@@ -237,7 +239,7 @@ def formula_from_models(models, atoms):
 
 @memo
 def formula_from_model(model, atoms):
-    """Creates a propositional formula from a (single) model.
+    """Creates a propositional formula from a single model.
        
        The input model must be an integer whose bits represent
        the truth values of atoms in a given sorted order.
@@ -274,7 +276,8 @@ def revise(K, R):
     S = completion(G)
     return S.formulas(1)
 
-def proj_merge(belief_bases, entailment_constraint=None):
+
+def merge(belief_bases, entailment_constraint=None):
     G = equibel.star_graph(len(belief_bases))
 
     if entailment_constraint:
