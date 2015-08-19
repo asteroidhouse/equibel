1522d4dd0cf5ccea4d3c07cf3fba811fabdcba26
Paul Vicol <paul22vicol@gmail.com>
2015-08-19 16:14:38 -0700

    Changed a couple test files

--- tests/asp_formatter_test.py
+++ tests/asp_formatter_test.py
@@ -1,35 +1,44 @@
-import equibel.format.ASP_Formatter as ASP_Formatter
-from equibel.EquibelGraph import EquibelGraph
+import equibel as eb
+from equibel import ASP_Formatter
+from equibel import EquibelGraph
+
+def get_atom_mapping(G):
+    atoms = [eb.Prop(atom) for atom in G.atoms()]
+    sorted_atoms = tuple(sorted(atoms))
+    return eb.create_atom_mapping(sorted_atoms)
 
 def test_asp_formatter():
-	G = EquibelGraph()
-	G.add_nodes([1,2,3])
-	G.add_edges([(1,2), (2,3)])
-	G.add_atom('a')
-	G.add_weight(1, 'b', 4)
+    G = eb.path_graph(3)
+    G.add_formula(0, "p & q")
+    G.add_formula(2, "~r")
 
-	print(ASP_Formatter.convert_to_asp(G))
+    atom_mapping = get_atom_mapping(G)
 
-	expected_result = \
+    print(eb.convert_to_asp(G, atom_mapping))
+
+    expected_result = \
 """node(1).
 node(2).
 node(3).
-atom(a).
-atom(b).
+edge(0,1).
+edge(1,0).
 edge(1,2).
 edge(2,1).
-edge(2,3).
-edge(3,2).
-weight(1,a,1).
-weight(1,b,4).
-weight(2,a,1).
-weight(2,b,1).
-weight(3,a,1).
-weight(3,b,1).
+weight(0,0,1).
+weight(0,1,1).
+weight(0,2,1).
+weight(1,0,1).
+weight(1,1,1).
+weight(1,2,1).
+weight(2,9,1).
+weight(2,1,1).
+weight(2,2,1).
+formula(0,and(2,1)).
+formula(2,neg(0)).
 """
 	
-	actual_result = ASP_Formatter.convert_to_asp(G)
-	assert(actual_result == expected_result)
+    actual_result = eb.convert_to_asp(G, atom_mapping)
+    assert(actual_result == expected_result)
 
 if __name__ == '__main__':
-	test_asp_formatter()
+    test_asp_formatter()
