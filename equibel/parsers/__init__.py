7d1aeb14364fc9bc11a3c72722120d93d432d1c2
Paul Vicol <paul22vicol@gmail.com>
2015-08-19 12:15:44 -0700

    Removed ASP_Parser import

--- equibel/parsers/__init__.py
+++ equibel/parsers/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-from equibel.parsers.ASP_Parser import *
 from equibel.parsers.FormulaParserASP import *
 from equibel.parsers.FormulaParserSim import *
 from equibel.parsers.formula_parser_infix import *
