2ffeb73e5f0d53b68bd29c65cfc7d90b9bbd37ce
Paul Vicol <paul22vicol@gmail.com>
2016-05-29 00:01:52 -0700

    Updated parsers

---
+++ equibel/parsers/bcfparser.py
@@ -0,0 +1,215 @@
+"""Parser for the Belief Change Format (BCF) representation of a graph and 
+associated scenario.
+
+An example of the contents of a file using the Belief Change Format is as follows:
+
+n 1..5
+a p q r
+
+f 1 (-p)
+f 3 (*(p q))
+f 3 (+(-p r))
+f 4 (+(*(p q) r))
+f 5 (*(-q r))
+
+e 1 2
+e 1 3
+e 3 1
+e 2 4
+e 4 2
+e 2 5
+e 5 2
+e 4 5
+"""
+#    Copyright (C) 2016
+#    Paul Vicol <pvicol@sfu.ca>
+#    All rights reserved.
+#    MIT license.
+from __future__ import absolute_import
+
+import sys
+import re
+
+from equibel.graph import EquibelGraph
+import equibel.parsers.FormulaParserSim as FormulaParserSim
+import equibel.formatters.BCF_Formatter as BCF_Formatter
+
+
+__all__ = ["parse_bcf"]
+
+
+class FormatError(Exception):
+    """
+    """
+    pass
+
+
+def process_node_line(tokens, graph, line_num):
+    """
+    """
+    # There are two cases for valid tokens:
+    #    1) The token can be an individual positive integer, like 3, or
+    #    2) The token can be a range of the form x..y, where x and y are
+    #      positive integers. To keep the parsing simple, whitespace is
+    #      not allowed anywhere in the token, so "3.. 5" is invalid.
+
+    if not tokens:
+        raise FormatError("Empty node line on line {0}".format(line_num))
+
+    for token in tokens:
+        if '..' in token:
+            m = re.match(r'([0-9]+)\.\.([0-9]+)', token)
+            if not m:
+                raise FormatError("Unrecognized range on line {0}: \"{1}\"".format(line_num, token))
+
+            range_start = int(m.group(1))
+            range_end   = int(m.group(2))
+
+            if range_start > range_end:
+                raise FormatError("End of range is smaller than start of range on line {0}: \"{1}\""
+                                  .format(line_num, token))
+
+            node_ids = range(range_start, range_end + 1)
+            graph.add_nodes(node_ids)
+        elif token.isdigit():
+            node_id = int(token)
+            graph.add_node(node_id)
+        else:
+            raise FormatError("Unrecognized node token on line {0}: \"{1}\"".format(line_num, token))
+
+
+def process_atom_line(tokens, graph, line_num):
+    """
+    """
+    if not tokens:
+        raise FormatError("Empty atom line on line {0}".format(line_num))
+
+    for token in tokens:
+        if not re.match(r'[a-zA-Z][a-zA-Z0-9]*', token):
+            raise FormatError("invalid atom name on line {0}: \"{1}\"".format(line_num, token))
+
+        graph.add_atom(token)
+
+
+def process_edge_line(tokens, graph, line_num):
+    """
+    """
+    if not tokens:
+        raise FormatError("Empty edge line on line {0}".format(line_num))
+    if len(tokens) != 2:
+        raise FormatError("Expected exactly 2 arguments in edge line on line {0}, got: \"{1}\""
+                          .format(line_num, ", ".join(tokens)))
+
+    from_node_str, to_node_str = tokens
+
+    if not from_node_str.isdigit():
+        raise FormatError("invalid node number on line {0}: \"{1}\"".format(line_num, from_node_str))
+    if not to_node_str.isdigit():
+        raise FormatError("invalid node number on line {0}: \"{1}\"".format(line_num, to_node_str))
+
+    from_node_id = int(from_node_str)
+    to_node_id = int(to_node_str)
+    graph.add_edge(from_node_id, to_node_id)
+
+
+def process_formula_line(node_str, formula_str, graph, line_num):
+    """
+    """
+    if not node_str.isdigit():
+        raise FormatError("Invalid node number on line {0}: \"{1}\"".format(line_num, node_str))
+    if not formula_str:
+        raise FormatError("No formula provided on line {0}".format(line_num))
+
+    node_id = int(node_str)
+    formula = FormulaParserSim.parse_formula(formula_str)
+    graph.add_formula(node_id, formula)
+
+
+process_line = {
+    'n': process_node_line,
+    'a': process_atom_line,
+    'e': process_edge_line,
+    'f': process_formula_line
+}
+
+def parse_bcf(filename):
+    """
+    """
+    f = open(filename, 'r')
+
+    # Creates the graph used to accumulate the information in the
+    # BCF file. As each line is parsed, this graph is updated to
+    # maintain the nodes, edges, atoms, weights, and formulas.
+    graph = EquibelGraph()
+
+    line_num = 1
+
+    line = f.readline()
+
+    while line:
+        tokens = line.split()
+
+        if tokens:
+            line_type = tokens[0]
+            if line_type in ['n', 'a', 'e']:
+                process_line[line_type](tokens[1:], graph, line_num)
+            elif line_type == 'f':
+                formula_start_line = line_num
+                if not tokens[1:]:
+                    raise FormatError("Empty formula line on line {0}".format(formula_start_line))
+                node_str = tokens[1]
+
+                formula_str = " ".join(tokens[2:])
+                while not balanced_parentheses(formula_str):
+                    next_line = f.readline()
+                    line_num += 1
+                    if not next_line:
+                        raise FormatError("Unbalanced parentheses on line {0}".format(line_num))
+
+                    # The space ensures that tokens on successive lines don't get compacted into
+                    # a single token, i.e. "p\n-q" becomes "p -q" instead of "p-q"
+                    formula_str += " " + next_line.strip()
+
+                process_formula_line(node_str, formula_str, graph, formula_start_line)
+            elif line_type == 'c':
+                pass
+            else:
+                raise FormatError("Unrecognized line type on line {0}: \"{1}\"".format(line_num, line_type))
+
+        line_num += 1
+        line = f.readline()
+
+    f.close()
+    return graph
+
+
+def balanced_parentheses(s):
+    stack = []
+    push_chars, pop_chars = "({[", ")}]"
+    for c in s:
+        if c in push_chars:
+            stack.append(c)
+        elif c in pop_chars:
+            if not len(stack):
+                return False
+            else:
+                stack_top = stack.pop()
+                balancing_bracket = push_chars[pop_chars.index(c)]
+                if stack_top != balancing_bracket:
+                    return False
+
+    return not len(stack)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: python BCF_Parser.py filename')
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    try:
+        graph = parse_bcf(filename)
+        print(BCF_Formatter.convert_to_bcf(graph))
+    except FormatError as err:
+        print(err)
---
+++ equibel/parsers/formulaparser.py
@@ -0,0 +1,197 @@
+"""Parser for propositional formulas represented using infix notation.
+
+The symbols used for the logical connectives are as follows:
+
+        ----------------------
+       | Connective | Symbol |
+       |----------------------
+       |    conj.   |    &   |
+       |    disj.   |    |   |
+       |   implies  |   ->   |
+       |    equiv   |    =   |
+       |     neg    |    ~   |
+        ----------------------
+
+The precedence and right/left associativity rules of the conectives are as follows:
+
+1. Negation (~) has the highest precendence, and is right-associative.
+2. Conjunction (&) has the next highest precedence, and is left-associative.
+3. Disjunction (|) is next, is left-associative.
+4. Implication (->) comes next, and is right-associative.
+5. Finally, equivalence (=) is last, and is right-associative.
+
+Using these precedence rules, the following formulas are equivalent:
+
+     p & q | r   ===   (p & q) | r
+    p & q -> r   ===   (p & q) -> r
+    p | ~r = q   ===   (p | (~r)) = q
+   ~p | ~q & r   ===   ((~p) | (~q)) & r
+
+The only importable function from this file is parse_formula, 
+which takes a string such as "p & q | ~r" and creates a Sympy logical
+formula object representing that formula.
+"""
+#    Copyright (C) 2016
+#    Paul Vicol <pvicol@sfu.ca>
+#    All rights reserved.
+#    MIT license.
+
+from __future__ import absolute_import
+
+import sys
+import logging
+
+import ply
+import ply.lex as lex
+import ply.yacc as yacc
+
+from sympy import symbols, simplify
+from sympy.logic.boolalg import *
+
+
+__all__ = ["parse_formula"]
+
+
+log = logging.getLogger('ply')
+
+
+##################################################################
+######                       LEXER                          ######
+##################################################################
+
+keywords = {"True": "TRUE", "False": "FALSE"}
+
+tokens = (["NEG", "AND", "OR", "IMPLIES", "EQUIV", "LPAREN", "RPAREN", 
+           "INTEGER", "IDENTIFIER"] + list(keywords.values()))
+
+def t_IDENTIFIER(t):
+    r"[_a-zA-Z][_a-zA-Z0-9]*"
+    t.type = keywords.get(t.value, "IDENTIFIER")
+    return t
+
+t_NEG = r"~"
+t_AND = r"&"
+t_OR = r"\|"
+t_IMPLIES = r"->"
+t_EQUIV = r"="
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_INTEGER = r"[0-9]+"
+
+t_ignore = " \t\n"
+
+
+def t_NEWLINE(t):
+    r"\n+"
+    t.lexer.lineno += len(t.value)
+    return t
+
+
+def t_error(t):
+    line = t.value.lstrip()
+    i = line.find("\n")
+    line = line if i == -1 else line[:i]
+    raise ValueError("Syntax error, line {0}: {1}".format(t.lineno + 1, line))
+
+
+##################################################################
+######                     PARSER                           ######
+##################################################################
+
+def p_FORMULA(p):
+    """FORMULA : ATOM
+               | BOOLEAN
+               | COMPOUND
+               | LPAREN FORMULA RPAREN"""
+    p[0] = p[1] if len(p) == 2 else p[2]
+
+
+def p_ATOM(p):
+    """ATOM : IDENTIFIER
+            | INTEGER"""
+    p[0] = symbols(p[1])
+
+
+def p_BOOLEAN_TRUE(p):
+    """BOOLEAN : TRUE"""
+    # true is a singleton object from sympy, of type sympy.logic.boolalg.BooleanTrue
+    p[0] = true
+
+
+def p_BOOLEAN_FALSE(p):
+    """BOOLEAN : FALSE"""
+    # false is a singleton object from sympy, of type sympy.logic.boolalg.BooleanFalse
+    p[0] = false
+
+def p_COMPOUND(p):
+    """COMPOUND : NEGATION
+                | CONJUNCTION
+                | DISJUNCTION
+                | IMPLICATION
+                | EQUIVALENCE"""
+    p[0] = p[1]
+
+
+def p_NEGATION(p):
+    """NEGATION : NEG FORMULA"""
+    p[0] = ~ p[2]
+
+
+def p_CONJUNCTION(p):
+    """CONJUNCTION : FORMULA AND FORMULA"""
+    p[0] = p[1] & p[3]
+
+
+def p_DISJUNCTION(p):
+    """DISJUNCTION : FORMULA OR FORMULA"""
+    p[0] = p[1] | p[3]
+
+
+def p_IMPLICATION(p):
+    """IMPLICATION : FORMULA IMPLIES FORMULA"""
+    # Material implication using sympy operator ">>"
+    p[0] = p[1] >> p[3]
+
+
+def p_EQUIVALENCE(p):
+    """EQUIVALENCE : FORMULA EQUIV FORMULA"""
+    #p[0] = Equivalent(p[1], p[3])
+    p[0] = (p[1] >> p[3]) & (p[3] >> p[1])
+
+
+def p_error(p):
+    if p is None:
+        raise ValueError("Unknown error")
+    raise ValueError("Syntax error, line {0}: {1}".format(p.lineno + 1, p.type))
+
+
+precedence = (("right", "EQUIV"),
+              ("right", "IMPLIES"),
+              ("left", "OR"),
+              ("left", "AND"),
+              ("right", "NEG"))
+
+#lexer = ply.lex.lex(optimize=1, debug=0)
+#parser = ply.yacc.yacc(debug=0, write_tables=0)
+lexer = lex.lex()
+parser = yacc.yacc(errorlog=log)
+
+
+def parse_formula(text):
+    try:
+        return parser.parse(text, lexer=lexer)
+    except ValueError as err:
+        raise ValueError(err)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: python formulaparser.py FORMULA_STRING')
+        sys.exit(1)
+
+    formula_str = sys.argv[1]
+    
+    formula = parse_infix_formula(formula_str)
+    
+    print(repr(formula))
+    print("Simplified = \n{0}".format(repr(simplify(formula))))
